41281ab687c23e2b99838aac38fe91b4bc37922b
brent <19ellisb@flintridgeprep.org>
2019-01-25 11:44:11 -0800

    for all n > 3 the program stalls

---
+++ ProblemThree.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.math.*;
+
+class ProblemThree {
+    public static void main(String testNum) {
+        BigInteger n = new BigInteger(testNum);
+        BigInteger i = new BigInteger("3");
+        ArrayList<BigInteger> primes = new ArrayList<BigInteger>();
+        primes.add(new BigInteger("2"));
+        
+        while (i.compareTo(n) == -1) {//for every i < n, is i prime, if yes add to primes
+            boolean iIsPrime = true;
+            for (BigInteger x: primes) {
+                if (!iIsPrime) { break; } //If i isnt prime, there's no need to keep checking
+                if(BigInteger.ZERO.compareTo(i.mod(x)) == 0) {//if i % x == 0
+                    iIsPrime = false;
+                }
+            }
+            if (iIsPrime) {
+                primes.add(i);
+            }
+            i.add(BigInteger.ONE);
+        }
+        
+        for (BigInteger x: primes) {
+            System.out.println(x.toString());
+        }
+    }
+}   
