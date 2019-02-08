3ca92437a9335bb49f4ff3050991aee983de4a3c
brent <19EllisB@flintridgeprep.org>
2019-02-08 09:37:41 -0800

    started 5

---
+++ ProblemFive.java
@@ -0,0 +1,11 @@
+
+class ProblemFive {
+    public static long main(long a) {
+        long[] divisors = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11};
+        long n = a;
+        outer: 
+        while (true) {
+            return n;
+        }   
+    }
+}   
